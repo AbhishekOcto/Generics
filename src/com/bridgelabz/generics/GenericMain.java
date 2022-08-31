320132b1d4ae3ab747c9bca2c6483fecc81b4cb3
Abhishek <you@example.com>
2022-08-31 19:48:00 +0530

    [Abhishek] Added: UC5 std out the max using printMax generic method

--- src/com/bridgelabz/generics/GenericMain.java
+++ src/com/bridgelabz/generics/GenericMain.java
@@ -3,10 +3,9 @@ package com.bridgelabz.generics;
 public class GenericMain {
 
 	public static void main(String[] args) {
-			System.out.println("Maximum of Integers is: " + FindMaximum.maxOfValues(101, 520, 188, 212));
-			System.out.println("Maximum of Float is: " + FindMaximum.maxOfValues(150.75f, 230.44f, 17.71f, 99.99f));
-			System.out.println("Maximum of three Strings is: " + FindMaximum.maxOfValues("aeroplane", "truck", "bicycle", "ferry"));
-		// TODO Auto-generated method stub
+		System.out.println("Maximum of Integers is: " + FindMaximum.maxOfValues(101, 520, 188, 212));
+		System.out.println("Maximum of Float is: " + FindMaximum.maxOfValues(150.75f, 230.44f, 17.71f, 99.99f));
+		System.out.println("Maximum of three Strings is: " + FindMaximum.maxOfValues("aeroplane", "truck", "bicycle", "ferry"));
 
 	}
 
