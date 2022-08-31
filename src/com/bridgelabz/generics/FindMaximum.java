8e71a0baafb60603df9b15f1da81c64793a21363
Abhishek <you@example.com>
2022-08-31 19:32:48 +0530

    [Abhishek] Added: UC4 taking more than 3 parameters using max method

--- src/com/bridgelabz/generics/FindMaximum.java
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.generics;
 
 /**
- * UC4
+ * UC4 Extend the max method to take more then three parameters
 * @author -Abhishek Kumar
-* @since - 26-08-2022
+* @since - 31-08-2022
 */
 
 public class FindMaximum {
@@ -13,21 +13,21 @@ public class FindMaximum {
 	* @param a1 : First value to compare
 	* @param a2 : Second value to compare
 	* @param a3 : Third value to compare
-	* @return max : Maximum of three values
+	* @param a4 : Fourth value to compare
+	* @return max : Maximum of four values
 	*/
-	public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+	public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3, T a4) {
 		T max = a1;
-		if (a2.compareTo(max) > 0)
-		max = a2;
-		if (a3.compareTo(max) > 0)
-		max = a3;
+		if (a2.compareTo(max) > 0 && a2.compareTo(a3) > 0 && a2.compareTo(a4) > 0) {
+			max = a2;
+		}
+		if (a3.compareTo(max) > 0 && a3.compareTo(a2) > 0 && a3.compareTo(a4) > 0) {
+			max = a3;
+		}
+		if (a4.compareTo(max) > 0 && a4.compareTo(a2) > 0 && a4.compareTo(a3) > 0) {
+			max = a4;
+		}
 		return max;
 		}
 	
-	public static void main(String[] args) {
-		System.out.println("Welcome To Find Maximum Problem Using Generics \n");
-		System.out.println("The maximum value between the three integer is : " + maxOfValues(31, 888, 517)+ "\n");
-		System.out.println("The maximum value between the three float is : " + maxOfValues(13.5f, 3.56f, 58.87f) + "\n");
-		System.out.println("The maximum value between the three string is : " + maxOfValues("apple", "banana", "peach"));
-		}
 }
